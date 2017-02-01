b5b3d84e4d6442325a30e1876dc81ea5ba8bbcf7
Paul Scherer <paul.scherer@mrc-epid.cam.ac.uk>
2017-02-01 16:05:30 +0000

    fix

--- pa_exemplar/summary_3rd_trimester.R
+++ pa_exemplar/summary_3rd_trimester.R
@@ -24,8 +24,8 @@ library(metafor)
 ########################### SET UP SERVERS  ###################################
 ###############################################################################
 # Set working directory to source our credentials
-#setwd("/home/l_pms69/exemplar_analyses/")
-setwd("/home/l_trpb2/git/exemplar_analyses/")
+setwd("/home/l_pms69/exemplar_analyses/")
+#setwd("/home/l_trpb2/git/exemplar_analyses/")
 
 # Sourcing the credentials sets values for the following variables:
 # server
