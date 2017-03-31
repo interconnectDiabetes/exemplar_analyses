a856e0e3f40761264aba3612ffafd06037d4da34
Paul Scherer <paul.scherer@mrc-epid.cam.ac.uk>
2017-03-31 12:08:59 +0100

    New Fish Exemplar with skeleton

---
+++ fish_exemplar/fish_analysis.R
@@ -0,0 +1,14 @@
+## Analysis script for first trimester PAIP analysis
+## Author: Paul Scherer
+##		   Tom Bishop
+## Date: 31/03/2017
+
+###############################################################################
+########################### Dependencies   ####################################
+###############################################################################
+library(opal)
+library(dsBaseClient)
+library(dsStatsClient)
+library(dsGraphicsClient)
+library(dsModellingClient)
+library(metafor)
