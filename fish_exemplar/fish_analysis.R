b958ea6d2d32085330c2cf6df7f34c9e2f2246d2
paulmorio <paul.scherer@mrc-epid.cam.ac.uk>
2017-05-25 00:27:10 +0100

    simple model 1 to test tomorrow

--- fish_exemplar/fish_analysis.R
+++ fish_exemplar/fish_analysis.R
@@ -220,7 +220,60 @@ my_covariate =  c("AGE_BASE". "SEX", "EDUCATION", "SMOKING", "PA", "FAM_DIAB", "
 
 REM_results = list()
 study_regs = data.frame()
-ref_table = 'E4'
+ref_table = 'D'
+
+mypath <- file.path('~','plots','model_1.png')
+png(file=mypath, width = 1260*length(my_exposure), height = 940*length(my_outcome), res = 300)
+par(mar=c(5,3,2,2)+0.1)
+par(mfrow=c(length(my_outcome),length(my_exposure)))
+
+for (k in 1:length(my_outcome)){
+	# Find outcome family for regression
+	out_class = ds.class(paste0(ref_table, '$', my_outcome[k]))[[1]]
+	if (out_class == 'factor') {
+		outcome_family = 'binomial'
+	}
+	else if (out_class == 'numeric' | out_class == 'integer') {
+		outcome_family = 'gaussian'
+	}
+
+	# for each exposure and
+	for (j in 1:length(my_exposure)){
+		estimates = vector()
+		s_errors = vector()
+		labels = vector()
+
+		for(i in 1:length(opals)) {
+			reg_data <- data.frame()
+
+			fmla <- as.formula(paste(ref_table, '$', my_outcome[k]," ~ ", paste0(c(paste0(ref_table, '$',my_exposure[j]), paste0(ref_table, '$',my_covariate)), collapse= "+")))
+			reg_data <- do_reg(fmla, names(opals[i]), my_outcome[k], outcome_family)
+
+			if (outcome_family == 'binomial' & length(reg_data) > 0){
+				reg_data = reg_data[1:9]
+				colnames(reg_data)[8] <- "low0.95CI"
+				colnames(reg_data)[9] <- "high0.95CI"    
+			}
+
+			study_regs = rbind(study_regs,reg_data)
+			estimates = rbind(estimates,reg_data[grep(my_exposure[j], reg_data$cov),"Estimate"])
+			s_errors = rbind(s_errors,reg_data[grep(my_exposure[j], reg_data$cov),"Std. Error"])
+			labels = rbind(labels, reg_data[2,1])      
+			variables = reg_data[grep(my_exposure[j], reg_data$cov), 'cov']
+		}
+
+		#meta analysis here
+		for (n in 1:length(variables)){
+			REM_results[[paste(c(my_outcome[k], my_exposure[j],my_covariate, variables[n],'REM'),collapse="_")]]  <- do_REM(estimates[,n], s_errors[,n], labels, fmla,out_family = outcome_family, variable = variables[n])
+		}
+	}
+}
+
+#Store results
+dev.off()
+model_1_all <- study_regs
+model_1_REM <- REM_results
+
 
 
 # +-+-+-+-+-+ +-+
