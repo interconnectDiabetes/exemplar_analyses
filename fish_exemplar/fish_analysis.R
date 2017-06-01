ba5b207247b8366c45fadcbe730612947a844ba0
Paul Scherer <paul.scherer@mrc-epid.cam.ac.uk>
2017-06-01 15:42:03 +0100

    removed older debug code into other file to keep review clean, and recommented model calls

--- fish_exemplar/fish_analysis.R
+++ fish_exemplar/fish_analysis.R
@@ -449,39 +449,14 @@ model_1_results = runRegModel(ref_table, my_exposure, my_outcome, my_covariate, 
 model_1_all = model_1_results[[1]]
 model_1_REM = model_1_results[[2]]
 
-# ## attempt with ds.lexis, ie the poisson piecewise regression
-# ds.lexis(data='D2', idCol='ID', entryCol='AGE_BASE', exitCol='AGE_END', statusCol='CASE_OBJ', newobj = "d1_expanded", datasources = opals)
-# ds.assign(toAssign='log(d1_expanded$SURVIVALTIME)', newobj='logSurvival')
-# ds.glm(formula='d1_expanded$CASE_OBJ~1+ d1_expanded$TIMEID+d1_expanded$AGE_END+d1_expanded$TOTAL', data='d1_expanded',family='poisson',offset='logSurvival')
-
-# datashield.assign(symbol = 'DANGER.nfilter.tab', value = quote(c(1)), opals = opals)
-# datashield.assign(symbol = 'DANGER.nfilter.glm', value = quote(c(1)), opals = opals)
-# 
-# 
-# ds.lexis.b(data='D2', intervalWidth=c(1,2,3), idCol='D2$ID', entryCol='D2$AGE_BASE', exitCol='D2$AGE_END', statusCol='D2$CASE_OBJ', expandDF = 'A')
-# ds.assign(toAssign='log(A$SURVTIME)', newobj='logSurvivalA')
-# 
-# ds.asNumeric('A$AGE_BASE','age')
-# ds.asNumeric('A$CENSOR','censor')
-# ds.asNumeric('A$TOTAL', 'total')
-# ds.asFactor('A$TIME.PERIOD','tid.f')
-# 
-# ds.glm(formula='censor~1+tid.f+age+total',family='poisson',offset='logSurvival')
-
-# # test purposes
-# ds.lexis.b(data='D', intervalWidth=c(1,2,3), idCol='D$ID', entryCol='D$entdate', exitCol='D$enddate', statusCol='D$censor', expandDF = 'A')
-# ds.lexis(data='D', idCol='ID', entryCol='entdate', exitCol='enddate', statusCol='censor', newobj = "d1_expanded", datasources = opals)
-# 
-
-
-# survival version short (case usage)
+# survival version short running normal lexis
 ref_table = 'D2'
 mypath = file.path('~', 'plots', 'model_1_surv.svg')
 model_1_surv = runSurvivalModel(ref_table, my_exposure, my_outcome, my_covariate, mypath)
 model_1_surv_all = model_1_surv[[1]]
 model_1_surv_all = model_1_surv[[2]]
 
-# survival version with lexis b (case usage)
+# survival version with lexis b 
 ref_table = 'D2'
 mypath = file.path('~', 'plots', 'model_1b_surv.svg')
 model_1_b = runSurvival_B_Model(ref_table, my_exposure, my_outcome, my_covariate, mypath, c(1,2,3))
