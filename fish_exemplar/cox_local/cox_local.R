379346e178ed20232a4efb5043d22c34ca14d2c7
paulmorio <paul.scherer@mrc-epid.cam.ac.uk>
2017-06-02 15:10:32 +0100

    rebased locally to see effect of change onto survival objects and cox regression

--- fish_exemplar/cox_local/cox_local.R
+++ fish_exemplar/cox_local/cox_local.R
@@ -2,7 +2,6 @@
 library(survival)
 
 setwd("~/workspace/exemplar_analyses/fish_exemplar/cox_local/")
-
 hmo <- read.csv("hmohiv.csv", row.names = 1, header = TRUE)
 
 # checking creation of survival object
@@ -14,3 +13,15 @@ coxfit1 = coxph(Surv(entdate, enddate, censor) ~ age + drug, hmo)
 coxfit2 = coxph(formula = as.formula(survivalObject~age+drug), hmo)
 
 coeffs1 = (summary(coxfit1))$coefficients
+
+## rebasing locally to 0 if that makes any difference here given that we create the survival object
+hmo$enddatenew = hmo$enddate - hmo$entdate
+hmo$entdatenew = 0
+
+# checking if there is difference between the survivalobjects created
+survivalObjectRebased = Surv(time = hmo$entdatenew, time2 = hmo$enddatenew, event = hmo$censor)
+str(survivalObjectRebased)
+identical(survivalObject, survivalObjectRebased)
+
+coxfitrebase = coxph(Surv(entdatenew, enddatenew, censor) ~ age + drug, hmo)
+coeffsrebase = (summary(coxfitrebase))$coefficients
