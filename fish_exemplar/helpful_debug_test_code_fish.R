3ee8c49b38c6c76b1fe6421740bedf657d083461
Paul Scherer <paul.scherer@mrc-epid.cam.ac.uk>
2017-06-05 13:06:27 +0100

    ds.lexis.o and its call in help

--- fish_exemplar/helpful_debug_test_code_fish.R
+++ fish_exemplar/helpful_debug_test_code_fish.R
@@ -10,7 +10,7 @@ ds.assign(toAssign='log(A$SURVTIME)', newobj='logSurvivalA')
 ds.lexis.b(data='D', intervalWidth=c(1,2,3), idCol='D$ID', entryCol='D$entdate', exitCol='D$enddate', statusCol='D$censor', expandDF = 'A')
 ds.lexis(data='D', idCol='ID', entryCol='entdate', exitCol='enddate', statusCol='censor', newobj = "d1_expanded", datasources = opals)
 
-
+## Lexis B
 # Working with cox cox data to test local against datashield.
 datashield.assign(symbol = 'DANGER.nfilter.tab', value = quote(c(1)), opals = opals)
 datashield.assign(symbol = 'DANGER.nfilter.glm', value = quote(c(1)), opals = opals)
@@ -42,6 +42,48 @@ ds.cbind(x=c('newenddate','D2'), newobj='D3')
 
 
 ds.lexis.b(data='D3', intervalWidth=c(90,90,90,500,500), idCol='D3$ID', entryCol='D3$zeros', exitCol='D3$newenddate', statusCol='D3$censor', expandDF = 'A3')
+ds.assign(toAssign='log(A3$SURVTIME)', newobj='logSurvivalA3')
+ds.asNumeric('A3$age','age3')
+ds.asNumeric('A3$CENSOR','censor3')
+ds.asNumeric('A3$drug', 'drug3')
+ds.asFactor('A3$TIME.PERIOD','tid.f3')
+
+ds.glm(formula='censor3~1+tid.f3+age3+drug3',family='poisson',offset='logSurvivalA3')
+
+
+## Lexis o
+# Working with cox cox data to test local against datashield.
+source(ds.lexis.o.R)
+datashield.assign(symbol = 'DANGER.nfilter.tab', value = quote(c(1)), opals = opals)
+datashield.assign(symbol = 'DANGER.nfilter.glm', value = quote(c(1)), opals = opals)
+
+ds.lexis.o(data='D', intervalWidth=c(90,90,90,500,500), idCol='D$ID', entryCol='D$entdate', exitCol='D$enddate', statusCol='D$censor', expandDF = 'A')
+ds.assign(toAssign='log(A$SURVTIME)', newobj='logSurvivalA')
+
+ds.asNumeric('A$age','age')
+ds.asNumeric('A$CENSOR','censor')
+ds.asNumeric('A$drug', 'drug')
+ds.asFactor('A$TIME.PERIOD','tid.f')
+
+
+ds.glm(formula='censor~1+tid.f+age+drug',family='poisson',offset='logSurvivalA')
+
+
+# with rebase
+all_participants_split <- ds.length('D$age',type = 'split')
+
+for(i in 1:length(opals)){
+  work1 <- all_participants_split[[i]]
+  work2 <- paste0("datashield.assign(opals[",i,"],'zeros', quote(rep(0,",work1,")))")
+  eval(parse(text=work2))
+}
+ds.cbind(x=c('zeros','D'), newobj='D2')
+
+ds.assign(toAssign = 'D$enddate-D$entdate', newobj = 'newenddate')
+ds.cbind(x=c('newenddate','D2'), newobj='D3')
+
+
+ds.lexis.o(data='D3', intervalWidth=c(90,90,90,500,500), idCol='D3$ID', entryCol='D3$zeros', exitCol='D3$newenddate', statusCol='D3$censor', expandDF = 'A3')
 ds.assign(toAssign='log(A3$SURVTIME)', newobj='logSurvivalA3')
 ds.asNumeric('A3$age','age3')
 ds.asNumeric('A3$CENSOR','censor3')
