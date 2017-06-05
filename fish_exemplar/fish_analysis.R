b2ead720c4eaeafd695b18673631e67be0cf4384
Paul Scherer <paul.scherer@mrc-epid.cam.ac.uk>
2017-06-05 15:23:33 +0100

    renamed serverside dataframes and commenting subsetting and missings counters as this interferes with analysis code dev at the moment

--- fish_exemplar/fish_analysis.R
+++ fish_exemplar/fish_analysis.R
@@ -48,39 +48,38 @@ study_names <- names(temp)
 num_studies <- length(temp)
 rm(temp)
 
-# remove participants with prevalent diabetes and type 1
-ds.subset(x = 'D', subset = 'D1', logicalOperator = 'PREV_DIAB<=', threshold = 1)
-noPrevalence <- ds.length('D1$SEX', type = 'split')
-ds.subset(x = 'D1', subset = 'D2', logicalOperator = 'TYPE_DIAB>=', threshold = 1)
-noPrevalence <- ds.length('D2$SEX', type = 'split')
+# # remove participants with prevalent diabetes and type 1
+# ds.subset(x = 'D', subset = 'E1', logicalOperator = 'PREV_DIAB<=', threshold = 1)
+# noPrevalence <- ds.length('E1$SEX', type = 'split')
+# ds.subset(x = 'E1', subset = 'E2', logicalOperator = 'TYPE_DIAB>=', threshold = 1)
+# noPrevalence <- ds.length('E2$SEX', type = 'split')
 
-# remove participants with too little and excessive consumption of calories
-ds.subset(x = 'D2', subset = 'D2a', logicalOperator = 'E_INTAKE>=', threshold = 3500)
-under3500cal <- ds.length('D2a$SEX', type = 'split')
-ds.subset(x = 'D2a', subset = 'D2b', logicalOperator = 'E_INTAKE<=', threshold = 500)
-afterIntake <- ds.length('D2b$SEX', type = 'split')
-
-
-# Loop to produce E4 and model_all_len for descriptive stats
-my_vars_all = c('TOTAL', 'NONFISH', 'FRESH', 'LEAN', 'FATTY', "SALT", "SSD", "FRIED", 'CASE_OBJ', "CASE_OBJ_SELF", "PREV_DIAB", "TYPE_DIAB", 
-	"AGE_BASE", "AGE_END","MI", "STROKE", "CANCER", "HYPERTENSION", "SEX", "BMI", "GEOG_AREA", "EDUCATION", "SMOKING", "PA", "ALCOHOL",
-	"FAM_DIAB", "E_INTAKE", "FRUIT", "VEG", "DAIRY", "FIBER", "RED_MEAT" , "PROC_MEAT", "SUG_BEVS", "MEDS", "WAIST", "SUPPLEMENTS")
-model_all_len <- data.frame()
-
-for (i in 2:length(my_vars_all)){
-  ds.subset(x = 'D2b', subset = 'E3', cols =  c(my_vars_all[1:i]))
-  ds.subset(x = 'E3', subset = 'E4', completeCases = TRUE)
-  model_all_len <- rbind(model_all_len,ds.length('E4$temp', type = 'split'))
-}
-
-row.names(model_all_len) <- my_vars_all[2:length(my_vars_all)]
+# # remove participants with too little and excessive consumption of calories
+# ds.subset(x = 'E2', subset = 'E3', logicalOperator = 'E_INTAKE>=', threshold = 3500)
+# under3500cal <- ds.length('E3$SEX', type = 'split')
+# ds.subset(x = 'E3', subset = 'E4', logicalOperator = 'E_INTAKE<=', threshold = 500)
+# afterIntake <- ds.length('E4$SEX', type = 'split')
 
 
-# Only Complete Cases (currently not in use for testing behaviour with nulls and the fact that complete 
-# cases knock out every available participant at the moment)
-ds.subset(x = 'D2b', subset = 'D3', completeCases = TRUE)
-complete_participants <- ds.length('D3$TOTAL')
-complete_participants_split <- ds.length('D3$TOTAL',type = 'split')
+# # Loop to produce E4 and model_all_len for descriptive stats
+# my_vars_all = c('TOTAL', 'NONFISH', 'FRESH', 'LEAN', 'FATTY', "SALT", "SSD", "FRIED", 'CASE_OBJ', "CASE_OBJ_SELF", "PREV_DIAB", "TYPE_DIAB", 
+# 	"AGE_BASE", "AGE_END","MI", "STROKE", "CANCER", "HYPERTENSION", "SEX", "BMI", "GEOG_AREA", "EDUCATION", "SMOKING", "PA", "ALCOHOL",
+# 	"FAM_DIAB", "E_INTAKE", "FRUIT", "VEG", "DAIRY", "FIBER", "RED_MEAT" , "PROC_MEAT", "SUG_BEVS", "MEDS", "WAIST", "SUPPLEMENTS")
+# model_all_len <- data.frame()
+
+# for (i in 2:length(my_vars_all)){
+#   ds.subset(x = 'E4', subset = 'E5', cols =  c(my_vars_all[1:i]))
+#   ds.subset(x = 'E5', subset = 'E6', completeCases = TRUE)
+#   model_all_len <- rbind(model_all_len,ds.length('E6$temp', type = 'split'))
+# }
+
+# row.names(model_all_len) <- my_vars_all[2:length(my_vars_all)]
+
+# # Only Complete Cases (currently not in use for testing behaviour with nulls and the fact that complete 
+# # cases knock out every available participant at the moment)
+# ds.subset(x = 'E6', subset = 'E7', completeCases = TRUE)
+# complete_participants <- ds.length('E7$TOTAL')
+# complete_participants_split <- ds.length('E7$TOTAL',type = 'split')
 
 ## TODO CHANGE ACCORDING TO TOP WHEN IT RUNS
 # Setup an additional proxy ID column for each study 
