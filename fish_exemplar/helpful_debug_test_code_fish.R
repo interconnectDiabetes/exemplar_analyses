74fef96307212a486c80b02715915489ed43d8cc
Paul Scherer <paul.scherer@mrc-epid.cam.ac.uk>
2017-06-05 11:43:05 +0100

    put code for cox cox data in seperate section to test local vs dtashield,.

--- fish_exemplar/helpful_debug_test_code_fish.R
+++ fish_exemplar/helpful_debug_test_code_fish.R
@@ -3,11 +3,19 @@ ds.lexis(data='D2', idCol='ID', entryCol='AGE_BASE', exitCol='AGE_END', statusCo
 ds.assign(toAssign='log(d1_expanded$SURVIVALTIME)', newobj='logSurvival')
 ds.glm(formula='d1_expanded$CASE_OBJ~1+ d1_expanded$TIMEID+d1_expanded$AGE_END+d1_expanded$TOTAL', data='d1_expanded',family='poisson',offset='logSurvival')
 
+ds.lexis.b(data='D2', intervalWidth=c(1,2,3), idCol='D2$ID', entryCol='D2$AGE_BASE', exitCol='D2$AGE_END', statusCol='D2$CASE_OBJ', expandDF = 'A')
+ds.assign(toAssign='log(A$SURVTIME)', newobj='logSurvivalA')
+
+# test purposes
+ds.lexis.b(data='D', intervalWidth=c(1,2,3), idCol='D$ID', entryCol='D$entdate', exitCol='D$enddate', statusCol='D$censor', expandDF = 'A')
+ds.lexis(data='D', idCol='ID', entryCol='entdate', exitCol='enddate', statusCol='censor', newobj = "d1_expanded", datasources = opals)
+
+
+# Working with cox cox data to test local against datashield.
 datashield.assign(symbol = 'DANGER.nfilter.tab', value = quote(c(1)), opals = opals)
 datashield.assign(symbol = 'DANGER.nfilter.glm', value = quote(c(1)), opals = opals)
 
-
-ds.lexis.b(data='D2', intervalWidth=c(1,2,3), idCol='D2$ID', entryCol='D2$AGE_BASE', exitCol='D2$AGE_END', statusCol='D2$CASE_OBJ', expandDF = 'A')
+ds.lexis.b(data='D', intervalWidth=c(1,2,3), idCol='D$ID', entryCol='D$entdate', exitCol='D$enddate', statusCol='D$censor', expandDF = 'A')
 ds.assign(toAssign='log(A$SURVTIME)', newobj='logSurvivalA')
 
 ds.asNumeric('A$AGE_BASE','age')
@@ -15,9 +23,5 @@ ds.asNumeric('A$CENSOR','censor')
 ds.asNumeric('A$TOTAL', 'total')
 ds.asFactor('A$TIME.PERIOD','tid.f')
 
+
 ds.glm(formula='censor~1+tid.f+age+total',family='poisson',offset='logSurvival')
-
-# test purposes
-ds.lexis.b(data='D', intervalWidth=c(1,2,3), idCol='D$ID', entryCol='D$entdate', exitCol='D$enddate', statusCol='D$censor', expandDF = 'A')
-ds.lexis(data='D', idCol='ID', entryCol='entdate', exitCol='enddate', statusCol='censor', newobj = "d1_expanded", datasources = opals)
-
