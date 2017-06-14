a29bc48cd35ed537dada185717c1cf118d16edd6
Paul Scherer <paul.scherer@mrc-epid.cam.ac.uk>
2017-06-14 15:14:45 +0100

    code for local checking of sweden's data, something is seriously wrong with it

---
+++ fish_exemplar/local_fish_analysis.R
@@ -0,0 +1,75 @@
+# Fish exemplar local testing
+
+setwd("V:/Studies/InterConnect/Internal/Fish exemplar/InterAct/for local testing")
+sweden <- read.csv("sweden.csv")
+
+noPrevalence = subset(x = sweden, PREV_DIAB == 0)
+noType1 = subset(x = noPrevalence, TYPE_DIAB == 2)
+intake = subset(x = noType1, E_INTAKE<=3500)
+intake2 = subset(x = intake, E_INTAKE>=500)
+
+intake2$EDUCATION = as.factor(intake2$EDUCATION)
+intake2$SMOKING = as.factor(intake2$SMOKING)
+  
+intake2$HYPERTENSION = unlist(lapply(X=intake2$HYPERTENSION, FUN = function(x){
+  if (x == -1){
+    a = NA
+  } else if (x == 0){
+    a = 0
+  } else if (x == 1){
+    a = 1
+  } 
+  return(a)
+}))
+
+
+intake2$STROKE = unlist(lapply(X=intake2$STROKE, FUN = function(x){
+  if (x == -1){
+    a = NA
+  } else if (x == 0){
+    a = 0
+  } else if (x == 1){
+    a = 1
+  } 
+  return(a)
+}))
+
+
+intake2$MI = unlist(lapply(X=intake2$MI, FUN = function(x){
+  if (x == -1){
+    a = NA
+  } else if (x == 0){
+    a = 0
+  } else if (x == 1){
+    a = 1
+  } 
+  return(a)
+}))
+
+intake2$STROKE = as.factor(intake2$STROKE)
+intake2$MI = as.factor(intake2$MI)
+intake2$HYPERTENSION = as.factor(intake2$HYPERTENSION)
+intake2$CASE_OBJ = as.factor(intake2$CASE_OBJ)
+
+(l <- sapply(intake2, function(x) is.factor(x)))
+m <- intake2[, l]
+ifelse(n <- sapply(m, function(x) length(levels(x))) == 1, "DROP", "NODROP")
+
+m = cbind(m, intake2$AGE_BASE)
+m = cbind(m, intake2$TOTAL)
+
+
+
+fmla = as.formula("CASE_OBJ~intake2$TOTAL + intake2$AGE_BASE + EDUCATION + SMOKING + STROKE + MI + HYPERTENSION")
+glm(formula = fmla, family = "binomial", data = m)
+
+
+
+
+
+
+
+
+
+
+
