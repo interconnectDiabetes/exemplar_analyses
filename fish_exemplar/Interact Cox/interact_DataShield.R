da68ab28f10ef55fd5848cf5c659bd1ecde46f09
Paul Scherer <paul.scherer@mrc-epid.cam.ac.uk>
2017-06-20 13:21:41 +0100

    We do not need the additional mediation/stratification models etc in this case

--- fish_exemplar/Interact Cox/interact_DataShield.R
+++ fish_exemplar/Interact Cox/interact_DataShield.R
@@ -50,7 +50,7 @@ rm(temp)
 
 # Setup an additional proxy ID column for each study 
 for(i in 1:length(opals)){
-  work1 <- afterIntake[[i]]
+  work1 <- all_participants_split[[i]]
   work2 <- paste0("datashield.assign(opals[",i,"],'ID', quote(c(1:",work1,")))")
   eval(parse(text=work2))
 }
@@ -59,7 +59,7 @@ ds.cbind(x=c('ID','D'), newobj='D2')
 
 # adding in zero columns to the studies
 for(i in 1:length(opals)){
-  work1 <- afterIntake[[i]]
+  work1 <- all_participants_split[[i]]
   work2 <- paste0("datashield.assign(opals[",i,"],'newStartDate', quote(rep(0,",work1,")))")
   eval(parse(text=work2))
 }
@@ -72,18 +72,6 @@ ds.cbind(x=c('newEndDate','D3'), newobj='D4')
 # ###############################################################################
 # ########################### FUNCTIONS  ########################################
 # ###############################################################################
-do_reg <- function(counter, my_fmla, study, outcome, out_family){
-	# performs a regular regression and returns the coefficients of the fitted model as a dataframe 
-	model <- ds.glm(formula = my_fmla, data = ref_table, family = out_family, datasources=opals[counter], maxit=100, checks=TRUE)
-	model_coeffs <- as.data.frame(model$coefficients)
-	model_coeffs$study = study
-	model_coeffs$outcome = outcome
-	model_coeffs$cov = rownames(model_coeffs)
-	for (x in 1:3){ model_coeffs <- model_coeffs[,c(ncol(model_coeffs),1:(ncol(model_coeffs)-1))]}
-	rownames(model_coeffs) = NULL
-	return(model_coeffs)
-}
-
 
 do_reg_survival <- function(counter, my_fmla, study, outcome, out_family, offset_column, lexisTable){
 	# performs a survival analysis using the formula on the appropiately lexised table
@@ -140,77 +128,6 @@ do_REM <- function(coeffs, s_err, labels, fmla, out_family, variable){
 		text(usr[1], usr[3], variable, adj = c( 0, 0),cex=0.75)
 	}
 	return(res)
-}
-
-findOutcomeFamily <- function(ref_table, outcome){
-	# Find outcome family for regression
-	# used in the do_reg function
-	out_class = ds.class(paste0(ref_table, '$', outcome))[[1]]
-	if (out_class == 'factor') {
-		outcome_family = 'binomial'
-	}
-	else if (out_class == 'numeric' | out_class == 'integer') {
-		outcome_family = 'gaussian'
-	}
-	return(outcome_family)
-}
-
-
-runRegModel <- function(ref_table, my_exposure, my_outcome, my_covariate, mypath){
-	# main function that runs, fits, and stores the results of a regression model using the 
-	# datashield process 
-	REM_results = list()
-	study_regs = data.frame()
-
-	svg(filename=mypath, 
-		width=4 * length(my_exposure), 
-		height=3 * length(my_outcome), 
-		pointsize=10)
-	par(mar=c(5,3,2,2)+0.1)
-	par(mfrow=c(length(my_outcome),length(my_exposure)))
-	par(ps=10)
-
-	for (k in 1:length(my_outcome)){
-		outcome_family = findOutcomeFamily(ref_table, my_outcome[k])
-
-		# for each exposure and
-		for (j in 1:length(my_exposure)){
-			estimates = vector()
-			s_errors = vector()
-			labels = vector()
-
-			for(i in 1:length(opals)) {
-				reg_data <- data.frame()
-
-				fmla <- as.formula(paste(ref_table, '$', my_outcome[k]," ~ ", paste0(c(paste0(ref_table, '$',my_exposure[j]), paste0(ref_table, '$',my_covariate)), collapse= "+")))
-				reg_data <- do_reg(i,fmla, names(opals[i]), my_outcome[k], outcome_family)
-
-				if (outcome_family == 'binomial' & length(reg_data) > 0){
-					reg_data = reg_data[1:9]
-					colnames(reg_data)[8] <- "low0.95CI"
-					colnames(reg_data)[9] <- "high0.95CI"    
-				}
-
-				study_regs = rbind(study_regs,reg_data)
-				estimates = rbind(estimates,reg_data[grep(my_exposure[j], reg_data$cov),"Estimate"])
-				s_errors = rbind(s_errors,reg_data[grep(my_exposure[j], reg_data$cov),"Std. Error"])
-				labels = rbind(labels, reg_data[2,1])      
-				variables = reg_data[grep(my_exposure[j], reg_data$cov), 'cov']
-			}
-
-			#meta analysis here
-			for (n in 1:length(variables)){
-				REM_results[[paste(c(my_outcome[k], my_exposure[j],my_covariate, variables[n],'REM'),collapse="_")]]  <- do_REM(estimates[,n], s_errors[,n], labels, fmla,out_family = outcome_family, variable = variables[n])
-			}
-		}
-	}
-
-	#Store results
-	dev.off()
-	model_all <- study_regs
-	model_rem <- REM_results
-
-	return (list(model_all, model_rem))
 }
 
 runSurvival_B_Model <- function(ref_table, my_exposure, my_outcome, my_covariate, mypath, interval_width) {
@@ -278,76 +195,6 @@ runSurvival_B_Model <- function(ref_table, my_exposure, my_outcome, my_covariate
 	model_rem <- REM_results
 
 	return (list(model_all, model_rem))
-}
-
-runIncrementalSurvivalModel <- function(ref_table, my_exposure, my_outcome, my_covariate, mypath_prefix, interval_width){
-	# Runs survival models incrementally through a list of provided covariates, producing randomeffectmodels and therein
-	# forest plots along the way. Mainly used for exploratory purposes.
-	REM_results = list()
-	study_regs = data.frame()
-	overall_df = data.frame()
-	for (i in 1:length(my_covariate)){
-		mypath_func = file.path(paste0(mypath_prefix, "_", i, "outta", length(my_covariate), ".svg"))
-		sub_covariate_list = my_covariate[1:i]
-		runResults = runSurvival_B_Model(ref_table, my_exposure, my_outcome, sub_covariate_list, mypath_func, c(2,2,2,2,2,2,2,2,2,2))
-		runCoeffs = runResults[[1]]
-		overall_df = rbind(overall_df, runCoeffs)
-		print(mypath_func)
-	}
-	return(overall_df)
-}
-
-runMediationModel <- function(ref_table, my_exposure, my_outcome, my_covariate, mypath_prefix, interval_width, my_mediation) {
-	# Runs a mediation survival model, with the provided extra mediations.
-	# ie. it does the following:
-	# 1. it runs the simple relationship outcome~exposure + covariates
-	# 2. it runs the relationship mediator~exposure + covariates
-	# 3. it runs the relationship outcome~mediator + covariates
-	# 4. it runs the relationship outcome~exposure + covariates + mediator
-	# make sure my_mediation is a vector with the string mediator
-
-	# outcome~exposure + covariates
-	mypath_1 = file.path(paste0(mypath_prefix, "_", "part_1", ".svg"))
-	mediate1 = runSurvival_B_Model(ref_table, my_exposure, my_outcome, my_covariate, mypath_1, interval_width)
-
-	# mediator~exposure + covariates
-	mypath_2 = file.path(paste0(mypath_prefix, "_", "part_2", ".svg"))
-	mediate2 = runSurvival_B_Model(ref_table, my_exposure, my_mediation, my_covariate, mypath_2, interval_width)
-	
-	# outcome~mediator + covariates
-	mypath_3 = file.path(paste0(mypath_prefix, "_", "part_3", ".svg"))
-	mediate3 = runSurvival_B_Model(ref_table, my_mediation, my_mediation, my_covariate, mypath_3, interval_width)
-
-	# outcome~exposure + covariates + mediator
-	my_covariate = c(my_covariate, my_mediation)
-	mypath_4 = file.path(paste0(mypath_prefix, "_", "part_4", ".svg"))
-	mediate4 = runSurvival_B_Model(ref_table, my_exposure, my_outcome, my_covariate, mypath_4, interval_width)
-
-	return(list(mediate1, mediate2, mediate3, mediate4))
-}
-
-runStratificationModel <- function(ref_table, my_exposure, my_outcome, my_covariate, mypath_prefix, interval_width, stratified_var) {
-	# Runs a stratified set of survival models given the categorical variable to be stratified
-	# could possibly be better implemented as a simple survival model where the input dataframe "ref_table" is
-	# instead just subset before hand, makes it easier to see in the analysis code when reading
-	# this function does the stratification automatically and runs a survival model on the newly stratified dataframe 
-
-	# elicit categories
-	cats = ds.summary(paste0('D$', stratified_var))[[1]]$categories # assumes that the categories of first study is shared
-	list_of_models = vector("list", length(cats))
-
-	for (category in 1:length(cats)){
-		newTable = paste0(ref_table, '_S_', category)
-		logicalOperatorString = paste0(stratified_var, '==')
-		ds.subset(x = ref_table, subset = newTable, logicalOperator = logicalOperatorString, threshold = cats[category])
-
-		# run the model on the stratified dataframe and append to list of models
-		mypath_func = file.path(paste0(mypath_prefix, "_",stratified_var,"==", cats[category], ".svg"))
-		stratified_model = runSurvival_B_Model(newTable, my_exposure, my_outcome, my_covariate, mypath_func, interval_width)
-		list_of_models[category] = stratified_model
-	}
-
-	return(list_of_models)
 }
 
 
