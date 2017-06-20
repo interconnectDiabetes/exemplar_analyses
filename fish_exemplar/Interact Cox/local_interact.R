3471cffc1167938ab7c004f397f6bad3d9241227
Paul Scherer <paul.scherer@mrc-epid.cam.ac.uk>
2017-06-20 12:39:31 +0100

    Indenting, and revariabling so that it can run on its own

--- fish_exemplar/Interact Cox/local_interact.R
+++ fish_exemplar/Interact Cox/local_interact.R
@@ -5,27 +5,18 @@ files <- dir('/home/l_trpb2/interact', full.names=TRUE)
 df.list <- lapply(files, read.csv)
 names(df.list) = c("denmark","france","germany","italy","netherlands","spain","sweden","uk")
 
-remove_age <- function(DF) {
-  
+remove_age <- function(DF) {  
   new_df = data.frame(DF, start = 0, end = DF$AGE_END_OBJ - DF$AGE_BASE)
   return(new_df)
 }
 
-
-df.list2 <- lapply(df.list, remove_age)
-
-
-
-
-fmla <- as.formula(paste("censor"," ~ ", '0', '+', 'tid.f', '+', paste0(c(paste0(lexised_table, '$',my_exposure[j]), paste0(lexised_table, '$',my_covariate)), collapse= "+")))
-
 cox_list <- function(DF) {
-  
   model <- summary(coxph(Surv(start, end, CASE_OBJ)~TOTAL+AGE_BASE+EDUCATION+SMOKING, DF))
   return(model)
-  
 }
 
+df.list2 <- lapply(df.list, remove_age)
+fmla <- as.formula(paste("censor"," ~ ", '0', '+', 'tid.f', '+', paste0(c(paste0(lexised_table, '$',my_exposure[j]), paste0(lexised_table, '$',my_covariate)), collapse= "+")))
 cox_out <- lapply(df.list2, cox_list)
 
 coeffs = unlist(lapply(cox_out, function (x) x['coefficients'][[1]][1]))
