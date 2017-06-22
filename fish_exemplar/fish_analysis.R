b98b6595e37152ee78b38e0c53b89f6104e482c8
Paul Scherer <paul.scherer@mrc-epid.cam.ac.uk>
2017-06-22 15:40:17 +0100

    sensitivity analyses involving family history

--- fish_exemplar/fish_analysis.R
+++ fish_exemplar/fish_analysis.R
@@ -482,11 +482,30 @@ model_2b_rem = model_2b[[2]]
 # Model 3: As model 2b + adj for BMI,  
 # Sensitivity analyses: include waist circumference or waist to hip ratio
 # 
-# Models to test Interaction 
+# Models to test Sensitivity (interaction)
+
+# interaction with fam_diab
 my_exposure = c('TOTAL')
+my_outcome = c('FAM_DIAB')
+my_covariate =  c("AGE_BASE", "SEX", "EDUCATION", "SMOKING", "PA","BMI", "COMORBID",
+                  "ALCOHOL", "FIBER", "MEAT", "FRUIT", "VEG", "SUG_BEVS", "E_INTAKE")
+
+ref_table = 'D4'
+mypath = file.path('~', 'plots', 'model_3c_surv.svg')
+model_2c = runSurvival_B_Model(ref_table, my_exposure, my_outcome, my_covariate, mypath, c(2))
+model_2c_all = model_2c[[1]]
+model_2c_rem = model_2c[[2]]
+
+my_exposure = c('FAM_DIAB')
 my_outcome = c('CASE_OBJ')
-my_covariate =  c("AGE_BASE", "SEX", "EDUCATION", "SMOKING", "PA", "FAM_DIAB", "MI", "STROKE", "CANCER", "HYPERTENSION",
-				"ALCOHOL", "FIBER", "PROC_MEAT", "FRUIT", "VEG", "SUG_BEVS", "SUPPLEMENTS", "E_INTAKE", "BMI")
+my_covariate =  c("AGE_BASE", "SEX", "EDUCATION", "SMOKING", "PA","BMI", "COMORBID",
+                  "ALCOHOL", "FIBER", "MEAT", "FRUIT", "VEG", "SUG_BEVS", "E_INTAKE")
+
+mypath = file.path('~', 'plots', 'model_3d_surv.svg')
+model_2d = runSurvival_B_Model(ref_table, my_exposure, my_outcome, my_covariate, mypath, c(2))
+model_2d_all = model_2d[[1]]
+model_2d_rem = model_2d[[2]]
+
 
 # interaction with waist
 my_exposure = c('TOTAL')
@@ -542,12 +561,13 @@ model_3d_rem = model_3d[[2]]
 # \_|  |_/\___/ \__,_|\___|_|     |_/
 # Exposure: total fish (g/d) at baseline*sex
 # Outcome: Type 2 diabetes incidence
-# Confounders: Age, sex, education, smoking, physical activity, family history of diabetes, MI, stroke, cancer, hypertension,  energy intake, fibre intake, processed meat intake, fruit and vegetables intake, sugary drinks intake, fish oil supplements, BMI
-# 
+# Confounders: Age, sex, education, smoking, physical activity, co-morbidities, BMI, energy intake, fibre intake, red and processed meat intake, fruit intake, vegetables intake, sugary drinks intake.
+
 # Stratified analyses by sex (men, women) if positive interaction 
+
 my_exposure = c('TOTAL')
 my_outcome = c('CASE_OBJ')
-my_covariate =  c("AGE_BASE", "SEX", "EDUCATION", "SMOKING", "PA", "FAM_DIAB", "MI", "STROKE", "CANCER", "HYPERTENSION",
+my_covariate =  c("AGE_BASE", "SEX", "EDUCATION", "SMOKING", "PA", "MI", "STROKE", "CANCER", "HYPERTENSION",
 				"ALCOHOL", "FIBER", "PROC_MEAT", "FRUIT", "VEG", "SUG_BEVS", "SUPPLEMENTS", "BMI")
 
 
