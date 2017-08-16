59f863dd9fcf39e6ca5f58ae7f29a9d6a38e3c71
Paul Scherer <paul.scherer@mrc-epid.cam.ac.uk>
2017-08-16 13:53:10 +0100

    More variables and exposures especially

--- fish_exemplar/fishExemplarDataExtraction.R
+++ fish_exemplar/fishExemplarDataExtraction.R
@@ -3,6 +3,182 @@
 
 setwd("V:/Studies/InterConnect/Internal/Other data sharing mechanisms/BioLINCC data_ US data/cardia")
 
+
+#  _____       _                                 
+# |  _  |     | |                                
+# | | | |_   _| |_ ___ ___  _ __ ___   ___  ___  
+# | | | | | | | __/ __/ _ \| '_ ` _ \ / _ \/ __| 
+# \ \_/ / |_| | || (_| (_) | | | | | |  __/\__ \ 
+#  \___/ \__,_|\__\___\___/|_| |_| |_|\___||___/ 
+
+# AGE_BASE
+dfa = read.csv2(file = "Y00/DATA/csv/aaref.csv", header = TRUE, sep = ",")
+dfb = read.csv2(file = "Y00/DATA/csv/aaf01.csv", header = TRUE, sep = ",")
+age_base = subset.data.frame(dfa, select = c(EXAMAGE, PID))
+age_base1 = subset.data.frame(dfb, select = c(A01AGE1, PID))
+
+# TYPE_DIAB
+
+# PREV_DIAB
+dfa = read.csv2(file = "Y00/DATA/csv/aaf08v2.csv", header = TRUE, sep = ",")
+dfb = read.csv2(file = "Y00/DATA/csv/aaf09gen.csv", header = TRUE, sep = ",")
+dfc = read.csv2(file = "Y00/DATA/csv/aachem.csv", header = TRUE, sep = ",")
+a08diab = subset.data.frame(dfa, select = c(A08DIAB, PID))
+a09dibst = subset.data.frame(dfb, select = c(A09DIBST, PID))
+al3glu = subset.data.frame(dfb, select = c(AL3_GLU, PID))
+
+
+# CASE_OBJ and CASE_OBJ_SELF
+dfa = read.csv2(file = "Y00/DATA/csv/baf08v2.csv", header = TRUE, sep = ",")
+dfb = read.csv2(file = "Y00/DATA/csv/baf09dib.csv", header = TRUE, sep = ",")
+dfc = read.csv2(file = "Y00/DATA/csv/baref.csv", header = TRUE, sep = ",")
+B08DIAB = subset.data.frame(dfa, select = c(B08DIAB, PID))
+B09DIBAG = subset.data.frame(dfb, select = c(B09DIBAG, PID))
+EXAMAGE = subset.data.frame(dfc, select = c(EXAMAGE, PID))
+
+dfd = read.csv2(file = "Y00/DATA/csv/caf08.csv", header = TRUE, sep = ",")
+dfe = read.csv2(file = "Y00/DATA/csv/caf08.csv", header = TRUE, sep = ",")
+dff = read.csv2(file = "Y00/DATA/csv/caref.csv", header = TRUE, sep = ",")
+C08DIAB = subset.data.frame(dfd, select = c(C08DIAB, PID))
+C08DIBAG = subset.data.frame(dfe, select = c(C08DIBAG, PID))
+EXAMAGE = subset.data.frame(dff, select = c(EXAMAGE, PID))
+
+dfg = read.csv2(file = "Y00/DATA/csv/daf08.csv", header = TRUE, sep = ",")
+dfh = read.csv2(file = "Y00/DATA/csv/daf08.csv", header = TRUE, sep = ",")
+dfi = read.csv2(file = "Y00/DATA/csv/daref.csv", header = TRUE, sep = ",")
+dfj = read.csv2(file = "Y00/DATA/csv/daglu.csv", header = TRUE, sep = ",")
+D08DIAB = subset.data.frame(dfg, select = c(D08DIBAG, PID))
+D08DIBAG = subset.data.frame(dfh, select = c(D08DIBAG, PID))
+EXAMAGE = subset.data.frame(dfi, select = c(EXAMAGE, PID))
+DL7GLU = subset.data.frame(dfj, select = c(DL7GLU, PID))
+
+dfk = read.csv2(file = "Y00/DATA/csv/eaf08.csv", header = TRUE, sep = ",")
+dfl = read.csv2(file = "Y00/DATA/csv/eaf08.csv", header = TRUE, sep = ",")
+dfm = read.csv2(file = "Y00/DATA/csv/earef.csv", header = TRUE, sep = ",")
+dfn = read.csv2(file = "Y00/DATA/csv/eaglu.csv", header = TRUE, sep = ",")
+dfo = read.csv2(file = "Y00/DATA/csv/eaglu.csv", header = TRUE, sep = ",")
+E08DIAB = subset.data.frame(dfk, select = c(E08DIAB, PID))
+E08DIBAG = subset.data.frame(dfl, select = c(E08DIBAG, PID))
+EXAMAGE = subset.data.frame(dfm, select = c(EXAMAGE, PID))
+EL7GLU = subset.data.frame(dfn, select = c(EL7GLU, PID))
+EL7GLU2H = subset.data.frame(dfo, select = c(EL7GLU2H, PID))
+
+dfp = read.csv2(file = "Y00/DATA/csv/faf08.csv", header = TRUE, sep = ",")
+dfq = read.csv2(file = "Y00/DATA/csv/faf08.csv", header = TRUE, sep = ",")
+dfr = read.csv2(file = "Y00/DATA/csv/faref.csv", header = TRUE, sep = ",")
+dfs = read.csv2(file = "Y00/DATA/csv/faglu.csv", header = TRUE, sep = ",")
+dft = read.csv2(file = "Y00/DATA/csv/faref.csv", header = TRUE, sep = ",")
+F08DIAB = subset.data.frame(dfp, select = c(F08DIAB, PID))
+F08DIBAG = subset.data.frame(dfq, select = c(F08DIBAG, PID))
+EXAMAGE = subset.data.frame(dfr, select = c(EXAMAGE, PID))
+FL7GLU = subset.data.frame(dfs, select = c(FL7GLU, PID))
+FDIABMED = subset.data.frame(dft, select = c(FDIABMED, PID))
+
+dfu = read.csv2(file = "Y00/DATA/csv/gaf08.csv", header = TRUE, sep = ",")
+dfv = read.csv2(file = "Y00/DATA/csv/gaf08.csv", header = TRUE, sep = ",")
+dfw = read.csv2(file = "Y00/DATA/csv/garef.csv", header = TRUE, sep = ",")
+dfx = read.csv2(file = "Y00/DATA/csv/gaglu.csv", header = TRUE, sep = ",")
+dfy = read.csv2(file = "Y00/DATA/csv/gaglu.csv", header = TRUE, sep = ",")
+G08DIAB = subset.data.frame(dfu, select = c(G08DIAB, PID))
+G08DIBAG = subset.data.frame(dfv, select = c(G08DIBAG, PID))
+EXAMAGE = subset.data.frame(dfw, select = c(EXAMAGE, PID))
+GL7GLU = subset.data.frame(dfx, select = c(GL7GLU, PID))
+GL7GLU2H = subset.data.frame(dfy, select = c(GL7GLU2H, PID))
+G83LDIA = NULL
+
+dfz = read.csv2(file = "Y00/DATA/csv/haf08.csv", header = TRUE, sep = ",")
+dfaa = read.csv2(file = "Y00/DATA/csv/haf08.csv", header = TRUE, sep = ",")
+dfab = read.csv2(file = "Y00/DATA/csv/haref.csv", header = TRUE, sep = ",")
+dfac = read.csv2(file = "Y00/DATA/csv/haglu.csv", header = TRUE, sep = ",")
+dfad = read.csv2(file = "Y00/DATA/csv/haglu.csv", header = TRUE, sep = ",")
+H08DIAB = subset.data.frame(dfaz, select = c(H08DIAB, PID))
+H08DIBAG = subset.data.frame(dfaa, select = c(H08DIBAG, PID))
+EXAMAGE = subset.data.frame(dfab, select = c(EXAMAGE, PID))
+HL7GLU = subset.data.frame(dfac, select = c(HL7GLU, PID))
+HL7GLU2H = subset.data.frame(dfad, select = c(HL7GLU2H, PID))
+H08MEDDIAA = NULL
+
+
+# AGE_END_OBJ 
+# AGE_END_OBJ_SELF
+dfd = read.csv2(file = "Y00/DATA/csv/baf09dib.csv", header = TRUE, sep = ",")
+dfe = read.csv2(file = "Y00/DATA/csv/baref.csv", header = TRUE, sep = ",")
+dff = read.csv2(file = "Y00/DATA/csv/caf08.csv", header = TRUE, sep = ",")
+dfd = read.csv2(file = "Y00/DATA/csv/caref.csv", header = TRUE, sep = ",")
+dfe = read.csv2(file = "Y00/DATA/csv/daf08.csv", header = TRUE, sep = ",")
+dff = read.csv2(file = "Y00/DATA/csv/daref.csv", header = TRUE, sep = ",")
+dfd = read.csv2(file = "Y00/DATA/csv/eaf08.csv", header = TRUE, sep = ",")
+dfe = read.csv2(file = "Y00/DATA/csv/earef.csv", header = TRUE, sep = ",")
+dff = read.csv2(file = "Y00/DATA/csv/faf08.csv", header = TRUE, sep = ",")
+dfd = read.csv2(file = "Y00/DATA/csv/faref.csv", header = TRUE, sep = ",")
+dfe = read.csv2(file = "Y00/DATA/csv/gaf08.csv", header = TRUE, sep = ",")
+dff = read.csv2(file = "Y00/DATA/csv/garef.csv", header = TRUE, sep = ",")
+dfd = read.csv2(file = "Y00/DATA/csv/haf08.csv", header = TRUE, sep = ",")
+dfe = read.csv2(file = "Y00/DATA/csv/haref.csv", header = TRUE, sep = ",")
+dff = read.csv2(file = "Y00/DATA/csv/baref.csv", header = TRUE, sep = ",")
+dfd = read.csv2(file = "Y00/DATA/csv/baref.csv", header = TRUE, sep = ",")
+dfe = read.csv2(file = "Y00/DATA/csv/caref.csv", header = TRUE, sep = ",")
+dff = read.csv2(file = "Y00/DATA/csv/caref.csv", header = TRUE, sep = ",")
+dfd = read.csv2(file = "Y00/DATA/csv/daref.csv", header = TRUE, sep = ",")
+dfe = read.csv2(file = "Y00/DATA/csv/daref.csv", header = TRUE, sep = ",")
+dff = read.csv2(file = "Y00/DATA/csv/earef.csv", header = TRUE, sep = ",")
+dfd = read.csv2(file = "Y00/DATA/csv/earef.csv", header = TRUE, sep = ",")
+dfe = read.csv2(file = "Y00/DATA/csv/faref.csv", header = TRUE, sep = ",")
+dff = read.csv2(file = "Y00/DATA/csv/faref.csv", header = TRUE, sep = ",")
+dff = read.csv2(file = "Y00/DATA/csv/garef.csv", header = TRUE, sep = ",")
+dfd = read.csv2(file = "Y00/DATA/csv/garef.csv", header = TRUE, sep = ",")
+dfe = read.csv2(file = "Y00/DATA/csv/haref.csv", header = TRUE, sep = ",")
+dff = read.csv2(file = "Y00/DATA/csv/haref.csv", header = TRUE, sep = ",")
+                                           
+#  _____                                         
+# |  ___|                                        
+# | |____  ___ __   ___  ___ _   _ _ __ ___  ___ 
+# |  __\ \/ / '_ \ / _ \/ __| | | | '__/ _ \/ __|
+# | |___>  <| |_) | (_) \__ \ |_| | | |  __/\__ \
+# \____/_/\_\ .__/ \___/|___/\__,_|_|  \___||___/
+#           | |                                  
+#           |_|                                  
+# FATTY
+dfa = read.csv2(file = "Y00/DATA/csv/aaf06fg.csv", header = TRUE, sep = ",")
+fatty = subset.data.frame(dfa, select = c(A06MFF0100, PID))
+
+# FRIED
+dfa = read.csv2(file = "Y00/DATA/csv/aaf06fg.csv", header = TRUE, sep = ",")
+fried = subset.data.frame(dfa, select = c(A06MFF0200, A06MSF0100, PID))
+
+# LEAN
+dfa = read.csv2(file = "Y00/DATA/csv/aaf06fg.csv", header = TRUE, sep = ",")
+lean = subset.data.frame(dfa, select = c(A06MFL0100, PID))
+
+# NONFISH
+dfa = read.csv2(file = "Y00/DATA/csv/aaf06fg.csv", header = TRUE, sep = ",")
+nonfish = subset.data.frame(dfa, select = c(A06MSL0100, PID))
+
+# TOTAL
+dfa = read.csv2(file = "Y00/DATA/csv/aaf06mj.csv", header = TRUE, sep = ",")
+total = subset.data.frame(dfa, select = c(A06FISH, PID))
+
+# ___  ___          _ _  __ _               
+# |  \/  |         | (_)/ _(_)              
+# | .  . | ___   __| |_| |_ _  ___ _ __ ___ 
+# | |\/| |/ _ \ / _` | |  _| |/ _ \ '__/ __|
+# | |  | | (_) | (_| | | | | |  __/ |  \__ \
+# \_|  |_/\___/ \__,_|_|_| |_|\___|_|  |___/
+# SEX
+dfa = read.csv2(file = "Y00/DATA/csv/aaf01.csv", header = TRUE, sep = ",")
+sex = subset.data.frame(dfa, select = c(A01SEX, PID))
+
+# BMI_CAT
+dfa = read.csv2(file = "Y00/DATA/csv/aaf20.csv", header = TRUE, sep = ",")
+bmicat = subset.data.frame(dfa, select = c(A20BMI, PID))
+             
+#  _____              __                      _               
+# /  __ \            / _|                    | |              
+# | /  \/ ___  _ __ | |_ ___  _   _ _ __   __| | ___ _ __ ___ 
+# | |    / _ \| '_ \|  _/ _ \| | | | '_ \ / _` |/ _ \ '__/ __|
+# | \__/\ (_) | | | | || (_) | |_| | | | | (_| |  __/ |  \__ \
+#  \____/\___/|_| |_|_| \___/ \__,_|_| |_|\__,_|\___|_|  |___/
+
 # Education a bit weird have to or them
 dfa = read.csv2(file = "Y00/DATA/csv/aaf01.csv", header = TRUE, sep = ",")
 dfb = read.csv2(file = "Y00/DATA/csv/aaf03.csv", header = TRUE, sep = ",")
@@ -77,3 +253,7 @@ meds = subset.data.frame(dfa, select = c(A08BPMED, PID))
 # WAIST
 dfa = read.csv2(file = "Y00/DATA/csv/aaf20.csv", header = TRUE, sep = ",")
 waist = subset.data.frame(dfa, select = c(A20WST, PID))
+
+#################################################################################################################
+#################################################################################################################
+#################################################################################################################
