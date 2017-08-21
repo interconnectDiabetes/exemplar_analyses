d6dbc1c5a1af53938f9344d5e510aeb9177898d6
Paul Scherer <paul.scherer@mrc-epid.cam.ac.uk>
2017-08-21 16:13:08 +0100

    Fix on numerals being read as characters changing them into factors. Did everything backwards

--- fish_exemplar/fishExemplarDataExtraction.R
+++ fish_exemplar/fishExemplarDataExtraction.R
@@ -1,6 +1,8 @@
 # This file is used to create a dataframe for the fish exemplar
 # Author: Paul Scherer
 
+library(tibble) # for easier dataframes, and rather the saving to dta
+library(haven)
 setwd("V:/Studies/InterConnect/Internal/Other data sharing mechanisms/BioLINCC data_ US data/cardia")
 
 
@@ -17,6 +19,9 @@ dfb = read.csv2(file = "Y00/DATA/csv/aaf01.csv", header = TRUE, sep = ",")
 EXAMAGE = subset.data.frame(dfa, select = c(EXAMAGE, PID))
 A01AGE1 = subset.data.frame(dfb, select = c(A01AGE1, PID))
 
+EXAMAGE$EXAMAGE = as.numeric(as.character(EXAMAGE$EXAMAGE))
+A01AGE1$A01AGE1 = as.numeric(as.character(A01AGE1$A01AGE1))
+
 # TYPE_DIAB
 
 # PREV_DIAB
@@ -29,9 +34,9 @@ AL3_GLU = subset.data.frame(dfc, select = c(AL3_GLU, PID))
 
 A08DIAB$A08DIAB = as.factor(A08DIAB$A08DIAB)
 levels(x = A08DIAB$A08DIAB) = c("No","Yes", "Not Sure")
-
 A09DIBST$A09DIBST = as.factor(A09DIBST$A09DIBST)
 levels(x = A09DIBST$A09DIBST) = c("Still Have","Under Control", "Cured or Gone") 
+AL3_GLU$AL3_GLU = as.numeric(as.character(AL3_GLU$AL3_GLU))
 
 # CASE_OBJ and CASE_OBJ_SELF
 dfa = read.csv2(file = "Y02/DATA/csv/baf08v2.csv", header = TRUE, sep = ",")
@@ -39,20 +44,23 @@ dfb = read.csv2(file = "Y02/DATA/csv/baf09dib.csv", header = TRUE, sep = ",")
 dfc = read.csv2(file = "Y02/DATA/csv/baref.csv", header = TRUE, sep = ",")
 B08DIAB = subset.data.frame(dfa, select = c(B08DIAB, PID))
 B09DIBAG = subset.data.frame(dfb, select = c(B09DIBAG, PID))
-EXAMAGE = subset.data.frame(dfc, select = c(EXAMAGE, PID))
+# EXAMAGE = subset.data.frame(dfc, select = c(EXAMAGE, PID))
 
 B08DIAB$B08DIAB = as.factor(B08DIAB$B08DIAB)
 levels(x = B08DIAB$B08DIAB) = c("No","Yes", "Not Sure")
+B09DIBAG$B09DIBAG = as.numeric(as.character(B09DIBAG$B09DIBAG))
+
 
 dfd = read.csv2(file = "Y05/DATA/csv/caf08.csv", header = TRUE, sep = ",")
 dfe = read.csv2(file = "Y05/DATA/csv/caf08.csv", header = TRUE, sep = ",")
 dff = read.csv2(file = "Y05/DATA/csv/caref.csv", header = TRUE, sep = ",")
 C08DIAB = subset.data.frame(dfd, select = c(C08DIAB, PID))
 C08DIBAG = subset.data.frame(dfe, select = c(C08DIBAG, PID))
-EXAMAGE = subset.data.frame(dff, select = c(EXAMAGE, PID))
+# EXAMAGE = subset.data.frame(dff, select = c(EXAMAGE, PID))
 
 C08DIAB$C08DIAB = as.factor(C08DIAB$C08DIAB)
 levels(x = C08DIAB$C08DIAB) = c("No","Yes", "Not Sure")
+C08DIBAG$C08DIBAG = as.numeric(as.character(C08DIBAG$C08DIBAG))
 
 dfg = read.csv2(file = "Y07/DATA/csv/daf08.csv", header = TRUE, sep = ",")
 dfh = read.csv2(file = "Y07/DATA/csv/daf08.csv", header = TRUE, sep = ",")
@@ -60,11 +68,14 @@ dfi = read.csv2(file = "Y07/DATA/csv/daref.csv", header = TRUE, sep = ",")
 dfj = read.csv2(file = "Y07/DATA/csv/daglu.csv", header = TRUE, sep = ",")
 D08DIAB = subset.data.frame(dfg, select = c(D08DIAB, PID))
 D08DIBAG = subset.data.frame(dfh, select = c(D08DIBAG, PID))
-EXAMAGE = subset.data.frame(dfi, select = c(EXAMAGE, PID))
+# EXAMAGE = subset.data.frame(dfi, select = c(EXAMAGE, PID))
 DL7GLU = subset.data.frame(dfj, select = c(DL7GLU, PID))
 
 D08DIAB$D08DIAB = as.factor(D08DIAB$D08DIAB)
 levels(x = D08DIAB$D08DIAB) = c("No","Yes", "Not Sure")
+D08DIBAG$D08DIBAG = as.numeric(as.character(D08DIBAG$D08DIBAG))
+DL7GLU$DL7GLU = as.numeric(as.character(DL7GLU$DL7GLU))
+
 
 dfk = read.csv2(file = "Y10/DATA/csv/eaf08.csv", header = TRUE, sep = ",")
 dfl = read.csv2(file = "Y10/DATA/csv/eaf08.csv", header = TRUE, sep = ",")
@@ -73,12 +84,16 @@ dfn = read.csv2(file = "Y10/DATA/csv/eaglu.csv", header = TRUE, sep = ",")
 dfo = read.csv2(file = "Y10/DATA/csv/eaglu.csv", header = TRUE, sep = ",")
 E08DIAB = subset.data.frame(dfk, select = c(E08DIAB, PID))
 E08DIBAG = subset.data.frame(dfl, select = c(E08DIBAG, PID))
-EXAMAGE = subset.data.frame(dfm, select = c(EXAMAGE, PID))
+# EXAMAGE = subset.data.frame(dfm, select = c(EXAMAGE, PID))
 EL7GLU = subset.data.frame(dfn, select = c(EL7GLU, PID))
 EL7GLU2H = subset.data.frame(dfo, select = c(EL7GLU2H, PID))
 
 E08DIAB$E08DIAB = as.factor(E08DIAB$E08DIAB)
 levels(x = E08DIAB$E08DIAB) = c("No","Yes", "Not Sure")
+E08DIBAG$E08DIBAG = as.numeric(as.character(E08DIBAG$E08DIBAG))
+EL7GLU$EL7GLU = as.numeric(as.character(EL7GLU$EL7GLU))
+EL7GLU2H$EL7GLU2H = as.numeric(as.character(EL7GLU2H$EL7GLU2H))
+
 
 dfp = read.csv2(file = "Y15/DATA/csv/faf08.csv", header = TRUE, sep = ",")
 dfq = read.csv2(file = "Y15/DATA/csv/faf08.csv", header = TRUE, sep = ",")
@@ -87,15 +102,16 @@ dfs = read.csv2(file = "Y15/DATA/csv/faglu.csv", header = TRUE, sep = ",")
 dft = read.csv2(file = "Y15/DATA/csv/faref.csv", header = TRUE, sep = ",")
 F08DIAB = subset.data.frame(dfp, select = c(F08DIAB, PID))
 F08DIBAG = subset.data.frame(dfq, select = c(F08DIBAG, PID))
-EXAMAGE = subset.data.frame(dfr, select = c(EXAMAGE, PID))
+# EXAMAGE = subset.data.frame(dfr, select = c(EXAMAGE, PID))
 FL7GLU = subset.data.frame(dfs, select = c(FL7GLU, PID))
 FDIABMED = subset.data.frame(dft, select = c(FDIABMED, PID))
 
 F08DIAB$F08DIAB = as.factor(F08DIAB$F08DIAB)
 levels(x = F08DIAB$F08DIAB) = c("No","Yes", "Not Sure")
-
 FDIABMED$FDIABMED = as.factor(FDIABMED$FDIABMED)
 levels(x = FDIABMED$FDIABMED) = c("No","Yes")
+F08DIBAG$F08DIBAG = as.numeric(as.character(F08DIBAG$F08DIBAG))
+FL7GLU$FL7GLU = as.numeric(as.character(FL7GLU$FL7GLU))
 
 dfu = read.csv2(file = "Y20/DATA/csv/gaf08.csv", header = TRUE, sep = ",")
 dfv = read.csv2(file = "Y20/DATA/csv/gaf08.csv", header = TRUE, sep = ",")
@@ -104,13 +120,16 @@ dfx = read.csv2(file = "Y20/DATA/csv/gaglu.csv", header = TRUE, sep = ",")
 dfy = read.csv2(file = "Y20/DATA/csv/gaglu.csv", header = TRUE, sep = ",")
 G08DIAB = subset.data.frame(dfu, select = c(G08DIAB, PID))
 G08DIBAG = subset.data.frame(dfv, select = c(G08DIBAG, PID))
-EXAMAGE = subset.data.frame(dfw, select = c(EXAMAGE, PID))
+# EXAMAGE = subset.data.frame(dfw, select = c(EXAMAGE, PID))
 GL7GLU = subset.data.frame(dfx, select = c(GL7GLU, PID))
 GL7GLU2H = subset.data.frame(dfy, select = c(GL7GLU2H, PID))
 G83LDIA = NULL
 
 G08DIAB$G08DIAB = as.factor(G08DIAB$G08DIAB)
 levels(x = G08DIAB$G08DIAB) = c("No","Yes", "Not Sure")
+G08DIBAG$G08DIBAG = as.numeric(as.character(G08DIBAG$G08DIBAG))
+GL7GLU$GL7GLU = as.numeric(as.character(GL7GLU$GL7GLU))
+GL7GLU2H$GL7GLU2H = as.numeric(as.character(GL7GLU2H$GL7GLU2H))
 
 dfz = read.csv2(file = "Y25/DATA/csv/haf08.csv", header = TRUE, sep = ",")
 dfaa = read.csv2(file = "Y25/DATA/csv/haf08.csv", header = TRUE, sep = ",")
@@ -119,86 +138,98 @@ dfac = read.csv2(file = "Y25/DATA/csv/haglu.csv", header = TRUE, sep = ",")
 dfad = read.csv2(file = "Y25/DATA/csv/haglu.csv", header = TRUE, sep = ",")
 H08DIAB = subset.data.frame(dfz, select = c(H08DIAB, PID))
 H08DIBAG = subset.data.frame(dfaa, select = c(H08DIBAG, PID))
-EXAMAGE = subset.data.frame(dfab, select = c(EXAMAGE, PID))
+# EXAMAGE = subset.data.frame(dfab, select = c(EXAMAGE, PID))
 HL7GLU = subset.data.frame(dfac, select = c(HL7GLU, PID))
 HL7GLU2H = subset.data.frame(dfad, select = c(HL7GLU2H, PID))
 H08MEDDIAA = NULL
 
 H08DIAB$H08DIAB = as.factor(H08DIAB$H08DIAB)
 levels(x = H08DIAB$H08DIAB) = c("No","Yes", "Not Sure")
+H08DIBAG$H08DIBAG = as.numeric(as.character(H08DIBAG$H08DIBAG))
+HL7GLU$HL7GLU = as.numeric(as.character(HL7GLU$HL7GLU))
+HL7GLU2H$HL7GLU2H = as.numeric(as.character(HL7GLU2H$HL7GLU2H))
+
+
 
 # AGE_END_OBJ 
 # AGE_END_OBJ_SELF
 dfa = read.csv2(file = "Y02/DATA/csv/baf09dib.csv", header = TRUE, sep = ",")
 dfb = read.csv2(file = "Y02/DATA/csv/baref.csv", header = TRUE, sep = ",")
-B09DIBAG = subset.data.frame(dfa, select = c(B09DIBAG, PID))
-EXAMAGE = subset.data.frame(dfb, select = c(EXAMAGE, PID))
+# B09DIBAG = subset.data.frame(dfa, select = c(B09DIBAG, PID))
+# EXAMAGE = subset.data.frame(dfb, select = c(EXAMAGE, PID))
 
 dfc = read.csv2(file = "Y05/DATA/csv/caf08.csv", header = TRUE, sep = ",")
 dfd = read.csv2(file = "Y05/DATA/csv/caref.csv", header = TRUE, sep = ",")
-C08DIBAG = subset.data.frame(dfc, select = c(C08DIBAG, PID))
-EXAMAGE = subset.data.frame(dfd, select = c(EXAMAGE, PID))
+# C08DIBAG = subset.data.frame(dfc, select = c(C08DIBAG, PID))
+# EXAMAGE = subset.data.frame(dfd, select = c(EXAMAGE, PID))
 
 dfe = read.csv2(file = "Y07/DATA/csv/daf08.csv", header = TRUE, sep = ",")
 dff = read.csv2(file = "Y07/DATA/csv/daref.csv", header = TRUE, sep = ",")
-D08DIBAG = subset.data.frame(dfe, select = c(D08DIBAG, PID))
-EXAMAGE = subset.data.frame(dff, select = c(EXAMAGE, PID))
+# D08DIBAG = subset.data.frame(dfe, select = c(D08DIBAG, PID))
+# EXAMAGE = subset.data.frame(dff, select = c(EXAMAGE, PID))
 
 dfg = read.csv2(file = "Y10/DATA/csv/eaf08.csv", header = TRUE, sep = ",")
 dfh = read.csv2(file = "Y10/DATA/csv/earef.csv", header = TRUE, sep = ",")
-E08DIBAG = subset.data.frame(dfg, select = c(E08DIBAG, PID))
-EXAMAGE = subset.data.frame(dfh, select = c(EXAMAGE, PID))
+# E08DIBAG = subset.data.frame(dfg, select = c(E08DIBAG, PID))
+# EXAMAGE = subset.data.frame(dfh, select = c(EXAMAGE, PID))
 
 dfi = read.csv2(file = "Y15/DATA/csv/faf08.csv", header = TRUE, sep = ",")
 dfj = read.csv2(file = "Y15/DATA/csv/faref.csv", header = TRUE, sep = ",")
-F08DIBAG = subset.data.frame(dfi, select = c(F08DIBAG, PID))
-EXAMAGE = subset.data.frame(dfj, select = c(EXAMAGE, PID))
+# F08DIBAG = subset.data.frame(dfi, select = c(F08DIBAG, PID))
+# EXAMAGE = subset.data.frame(dfj, select = c(EXAMAGE, PID))
 
 dfk = read.csv2(file = "Y20/DATA/csv/gaf08.csv", header = TRUE, sep = ",")
 dfl = read.csv2(file = "Y20/DATA/csv/garef.csv", header = TRUE, sep = ",")
-G08DIBAG = subset.data.frame(dfk, select = c(G08DIBAG, PID))
-EXAMAGE = subset.data.frame(dfl, select = c(EXAMAGE, PID))
+# G08DIBAG = subset.data.frame(dfk, select = c(G08DIBAG, PID))
+# EXAMAGE = subset.data.frame(dfl, select = c(EXAMAGE, PID))
 
 dfm = read.csv2(file = "Y25/DATA/csv/haf08.csv", header = TRUE, sep = ",")
 dfn = read.csv2(file = "Y25/DATA/csv/haref.csv", header = TRUE, sep = ",")
-H08DIBAG = subset.data.frame(dfm, select = c(H08DIBAG, PID))
-EXAMAGE = subset.data.frame(dfn, select = c(EXAMAGE, PID))
+# H08DIBAG = subset.data.frame(dfm, select = c(H08DIBAG, PID))
+# EXAMAGE = subset.data.frame(dfn, select = c(EXAMAGE, PID))
 
 dfo = read.csv2(file = "Y02/DATA/csv/baref.csv", header = TRUE, sep = ",")
 dfp = read.csv2(file = "Y02/DATA/csv/baref.csv", header = TRUE, sep = ",")
 EX2_AGE = subset.data.frame(dfo, select = c(EX2_AGE, PID))
-EXAMAGE = subset.data.frame(dfp, select = c(EXAMAGE, PID))
+# EXAMAGE = subset.data.frame(dfp, select = c(EXAMAGE, PID))
+EX2_AGE$EX2_AGE = as.numeric(as.character(EX2_AGE$EX2_AGE))
 
 dfq = read.csv2(file = "Y05/DATA/csv/caref.csv", header = TRUE, sep = ",")
 dfr = read.csv2(file = "Y05/DATA/csv/caref.csv", header = TRUE, sep = ",")
 EX3_AGE = subset.data.frame(dfq, select = c(EX3_AGE, PID))
-EXAMAGE = subset.data.frame(dfr, select = c(EXAMAGE, PID))
+# EXAMAGE = subset.data.frame(dfr, select = c(EXAMAGE, PID))
+EX3_AGE$EX3_AGE = as.numeric(as.character(EX3_AGE$EX3_AGE))
 
 dfs = read.csv2(file = "Y07/DATA/csv/daref.csv", header = TRUE, sep = ",")
 dft = read.csv2(file = "Y07/DATA/csv/daref.csv", header = TRUE, sep = ",")
 EX4_AGE = subset.data.frame(dfs, select = c(EX4_AGE, PID))
-EXAMAGE = subset.data.frame(dft, select = c(EXAMAGE, PID))
+# EXAMAGE = subset.data.frame(dft, select = c(EXAMAGE, PID))
+EX4_AGE$EX4_AGE = as.numeric(as.character(EX4_AGE$EX4_AGE))
 
 dfu = read.csv2(file = "Y10/DATA/csv/earef.csv", header = TRUE, sep = ",")
 dfv = read.csv2(file = "Y10/DATA/csv/earef.csv", header = TRUE, sep = ",")
 EX5_AGE = subset.data.frame(dfu, select = c(EX5_AGE, PID))
-EXAMAGE = subset.data.frame(dfv, select = c(EXAMAGE, PID))
+# EXAMAGE = subset.data.frame(dfv, select = c(EXAMAGE, PID))
+EX5_AGE$EX5_AGE = as.numeric(as.character(EX5_AGE$EX5_AGE))
 
 dfw = read.csv2(file = "Y15/DATA/csv/faref.csv", header = TRUE, sep = ",")
 dfx = read.csv2(file = "Y15/DATA/csv/faref.csv", header = TRUE, sep = ",")
 EX6_AGE = subset.data.frame(dfw, select = c(EX6_AGE, PID))
-EXAMAGE = subset.data.frame(dfx, select = c(EXAMAGE, PID))
+# EXAMAGE = subset.data.frame(dfx, select = c(EXAMAGE, PID))
+EX6_AGE$EX6_AGE = as.numeric(as.character(EX6_AGE$EX6_AGE))
 
 dfy = read.csv2(file = "Y20/DATA/csv/garef.csv", header = TRUE, sep = ",")
 dfz = read.csv2(file = "Y20/DATA/csv/garef.csv", header = TRUE, sep = ",")
 EX7_AGE = subset.data.frame(dfy, select = c(EX7_AGE, PID))
-EXAMAGE = subset.data.frame(dfz, select = c(EXAMAGE, PID))
+# EXAMAGE = subset.data.frame(dfz, select = c(EXAMAGE, PID))
+EX7_AGE$EX7_AGE = as.numeric(as.character(EX7_AGE$EX7_AGE))
 
 dfaa = read.csv2(file = "Y25/DATA/csv/haref.csv", header = TRUE, sep = ",")
 dfab = read.csv2(file = "Y02/DATA/csv/baref.csv", header = TRUE, sep = ",")
 EX8_AGE = subset.data.frame(dfaa, select = c(EX8_AGE, PID))
-EXAMAGE = subset.data.frame(dfab, select = c(EXAMAGE, PID))
-                                          
+# EXAMAGE = subset.data.frame(dfab, select = c(EXAMAGE, PID))
+EX8_AGE$EX8_AGE = as.numeric(as.character(EX8_AGE$EX8_AGE))
+                                         
 # _____                                         
 # |  ___|                                        
 # | |____  ___ __   ___  ___ _   _ _ __ ___  ___ 
@@ -210,22 +241,27 @@ EXAMAGE = subset.data.frame(dfab, select = c(EXAMAGE, PID))
 # FATTY
 dfa = read.csv2(file = "Y00/DATA/csv/aaf06fg.csv", header = TRUE, sep = ",")
 fatty = subset.data.frame(dfa, select = c(A06MFF0100, PID))
-
+fatty$A06MFF0100 = as.numeric(as.character(fatty$A06MFF0100))
 # FRIED
 dfa = read.csv2(file = "Y00/DATA/csv/aaf06fg.csv", header = TRUE, sep = ",")
 fried = subset.data.frame(dfa, select = c(A06MFF0200, A06MSF0100, PID))
+fried$A06MFF0200 = as.numeric(as.character(fried$A06MFF0200))
+fried$A06MSF0100 = as.numeric(as.character(fried$A06MSF0100))
 
 # LEAN
 dfa = read.csv2(file = "Y00/DATA/csv/aaf06fg.csv", header = TRUE, sep = ",")
 lean = subset.data.frame(dfa, select = c(A06MFL0100, PID))
+lean$A06MFL0100 = as.numeric(as.character(lean$A06MFL0100))
 
 # NONFISH
 dfa = read.csv2(file = "Y00/DATA/csv/aaf06fg.csv", header = TRUE, sep = ",")
 nonfish = subset.data.frame(dfa, select = c(A06MSL0100, PID))
+nonfish$A06MSL0100 = as.numeric(as.character(nonfish$A06MSL0100))
 
 # TOTAL
 dfa = read.csv2(file = "Y00/DATA/csv/aaf06mj.csv", header = TRUE, sep = ",")
 total = subset.data.frame(dfa, select = c(A06FISH, PID))
+total$A06FISH = as.numeric(as.character(total$A06FISH))
 
 # ___  ___          _ _  __ _               
 # |  \/  |         | (_)/ _(_)              
@@ -242,6 +278,7 @@ levels(x = A01SEX$A01SEX) = c("Male","Female")
 # BMI
 dfa = read.csv2(file = "Y00/DATA/csv/aaf20.csv", header = TRUE, sep = ",")
 A20BMI = subset.data.frame(dfa, select = c(A20BMI, PID))
+total$A06FISH = as.numeric(as.character(total$A06FISH))
              
 #  _____              __                      _               
 # /  __ \            / _|                    | |              
@@ -273,10 +310,12 @@ A01SMNOW$A01SMNOW = as.factor(A01SMNOW$A01SMNOW)
 # PA
 dfa = read.csv2(file = "Y00/DATA/csv/aaf19.csv", header = TRUE, sep = ",")
 A19MODWK = subset.data.frame(dfa, select = c(A19MODWK, PID))
+A19MODWK$A19MODWK = as.numeric(as.character(A19MODWK$A19MODWK))
 
 # ALCOHOL
 dfa = read.csv2(file = "Y00/DATA/csv/aaf06a.csv", header = TRUE, sep = ",")
 A06ALCHL = subset.data.frame(dfa, select = c(A06ALCHL, PID))
+A06ALCHL$A06ALCHL = as.numeric(as.character(A06ALCHL$A06ALCHL))
 
 # FAM_DIAB
 dfa = read.csv2(file = "Y00/DATA/csv/aaf11.csv", header = TRUE, sep = ",")
@@ -315,30 +354,41 @@ levels(x = A08HBP$A08HBP) = c("No","Yes", "Not Sure")
 # E_INTAKE
 dfa = read.csv2(file = "Y00/DATA/csv/aaf06a.csv", header = TRUE, sep = ",")
 A06CALO = subset.data.frame(dfa, select = c(A06CALO, PID))
+A06CALO$A06CALO = as.numeric(as.character(A06CALO$A06CALO))
 
 # FRUIT
 dfa = read.csv2(file = "Y00/DATA/csv/aaf06mj.csv", header = TRUE, sep = ",")
 A06FRUIT = subset.data.frame(dfa, select = c(A06FRUIT, PID))
+A06FRUIT$A06FRUIT = as.numeric(as.character(A06FRUIT$A06FRUIT))
 
 # VEG
 dfa = read.csv2(file = "Y00/DATA/csv/aaf06mj.csv", header = TRUE, sep = ",")
 A06VEGETABLE = subset.data.frame(dfa, select = c(A06VEGETABLE, PID))
+A06VEGETABLE$A06VEGETABLE = as.numeric(as.character(A06VEGETABLE$A06VEGETABLE))
 
 # FIBER
 dfa = read.csv2(file = "Y00/DATA/csv/aaf06a.csv", header = TRUE, sep = ",")
 A06FIBER = subset.data.frame(dfa, select = c(A06FIBER, PID))
+A06FIBER$A06FIBER = as.numeric(as.character(A06FIBER$A06FIBER))
 
 # RED_MEAT
 dfa = read.csv2(file = "Y00/DATA/csv/aaf06fg.csv", header = TRUE, sep = ",")
 red = subset.data.frame(dfa, select = c(A06MRF0100,A06MRF0200,A06MRF0300, A06MRF0400, PID))
+red$A06MRF0100 = as.numeric(as.character(red$A06MRF0100))
+red$A06MRF0200 = as.numeric(as.character(red$A06MRF0200))
+red$A06MRF0300 = as.numeric(as.character(red$A06MRF0300))
+red$A06MRF0400 = as.numeric(as.character(red$A06MRF0400))
 
 # PROC-MEAT
 dfa = read.csv2(file = "Y00/DATA/csv/aaf06fg.csv", header = TRUE, sep = ",")
 proc = subset.data.frame(dfa, select = c(A06MCF0200,A06MCF0100 , PID))
+proc$A06MCF0200 = as.numeric(as.character(proc$A06MCF0200))
+proc$A06MCF0100 = as.numeric(as.character(proc$A06MCF0100))
 
 # SUG_BEVS
 dfa = read.csv2(file = "Y00/DATA/csv/aaf06fg.csv", header = TRUE, sep = ",")
 A06BVS0400 = subset.data.frame(dfa, select = c(A06BVS0400, PID))
+A06BVS0400$A06BVS0400 = as.numeric(as.character(A06BVS0400$A06BVS0400))
 
 # MEDS
 dfa = read.csv2(file = "Y00/DATA/csv/aaf08v2.csv", header = TRUE, sep = ",")
@@ -349,6 +399,7 @@ levels(x = A08BPMED$A08BPMED) = c("No","Yes", "Not Sure")
 # WAIST
 dfa = read.csv2(file = "Y00/DATA/csv/aaf20.csv", header = TRUE, sep = ",")
 A20WST = subset.data.frame(dfa, select = c(A20WST, PID))
+A20WST$A20WST = as.numeric(as.character(A20WST$A20WST))
 
 rm(dfa, dfb, dfc, dfd, dfe, dff, dfg, dfh, dfi, dfj, dfk, dfl, dfm, dfn, dfo, dfp, dfq, 
 	dfr, dfs, dft, dfu, dfv, dfw, dfx, dfy, dfz, dfaa, dfab, dfac, dfad)
@@ -365,7 +416,10 @@ dataframes_list = list(EXAMAGE,A01AGE1,A08DIAB,A09DIBST,AL3_GLU,B08DIAB,B09DIBAG
 	A06FRUIT,A06VEGETABLE,A06FIBER,red,proc,A06BVS0400,A08BPMED,A20WST)
 
 cardia = Reduce(function(...) merge(..., by="PID", all=TRUE), dataframes_list)
-
 colnames(cardia)[1] <- "ID"
 
+cardia_tibble = as_tibble(cardia)
 save(cardia,file="V:/Studies/InterConnect/Internal/Other data sharing mechanisms/BioLINCC data_ US data/cardia/cardia_r_df.Rdata")
+write_dta(data = cardia_tibble, path = "V:/Studies/InterConnect/Internal/Other data sharing mechanisms/BioLINCC data_ US data/cardia/cardia_new.dta")
+
+
